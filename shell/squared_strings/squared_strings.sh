6bd8e08745c9867bb6666b380488cb7a9655715f
Diego Braga <dsbrgg@gmail.com>
2019-05-23 17:48:04 +0100

    do vertical mirror in shell exercise

---
+++ shell/squared_strings/squared_strings.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+vertMirror() {
+  s=$1
+  p=${s//\\n/}
+  r=""
+
+  for (( i = 0; i <= ${#p} - 4; i += 4 ))
+  do
+    if [[ $i -eq 0 ]]; then
+      r="$(echo ${p:i:4} | rev)"
+    else
+      r="${r}\n$(echo ${p:i:4} | rev)"
+    fi
+  done
+
+  echo $r
+}
+
+oper() {
+  $1 $2
+}
+
+oper $1 $2
