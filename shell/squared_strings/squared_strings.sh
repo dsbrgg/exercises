bcde8de2c768e4b541ddb48de223c3962f050305
Diego Braga <dsbrgg@gmail.com>
2019-05-25 15:24:18 +0100

    fuck shell higher order functions

--- shell/squared_strings/squared_strings.sh
+++ shell/squared_strings/squared_strings.sh
@@ -2,15 +2,16 @@
 
 vertMirror() {
   s=$1
-  p=${s//\\n/}
+  l=$(echo $1 | egrep -o '(\w*)' | tr -d '\n')
+  p=${s//,/}
   r=""
 
-  for (( i = 0; i <= ${#p} - 4; i += 4 ))
+  for (( i = 0; i <= ${#p} - ${#l}; i += ${#l} ))
   do
     if [[ $i -eq 0 ]]; then
-      r="$(echo ${p:i:4} | rev)"
+      r="$(echo ${p:i:${#l}} | rev)"
     else
-      r="${r}\n$(echo ${p:i:4} | rev)"
+      r="${r}\r$(echo ${p:i:${#l}} | rev)"
     fi
   done
 
@@ -20,22 +21,23 @@ vertMirror() {
 horMirror() {
   s=$1
   p=${s//,/}
+  l=$(echo $1 | egrep -o '(\w*)' | tr -d '\n')
   a=()
   r=""
 
-  for (( i = ${#p}; i >= 0; i -= 4 ))
+  for (( i = ${#p}; i >= 0; i -= ${#l} ))
   do
     if [[ $i -gt 0 ]]; then
-      a+=(${p:i-4:4})
+      a+=(${p:i-${#l}:${#l}})
     fi
   done
 
-  l=${#a[@]}
+  al=${#a[@]}
 
   for e in "${a[@]}";
   do
-    if [[ $e != ${a[@]:l-1} ]]; then
-      r="${r}${e}\n"
+    if [[ $e != ${a[@]:al-1} ]]; then
+      r="${r}${e}\r"
     else
       r="${r}${e}"
     fi
@@ -47,5 +49,17 @@ horMirror() {
 oper() {
   $1 $2
 }
+# Solution found, way better
+# vertMirror()
+# {
+#   out=""
+#   for a in $(echo $1 | tr ',' ' '); do
+#     out=$out'\r'$(echo $a | rev)
+#   done
+#   echo -e $out
+# }
+# horMirror(){ vertMirror $1 $2 | rev; }
+# oper(){ eval $1 $2; }
+# oper $1 $2
 
 oper $1 $2
